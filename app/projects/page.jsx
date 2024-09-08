51fb3fc62dc7451629d2d4a9d858b3174134bacb
Voiduh <jpik98@yahoo.com>
2024-09-07 21:06:20 -0400

    Update

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -6,35 +6,43 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 const githubProjects = [
   {
+    title: "Portfolio Site",
+    link: "https://github.com/Voiduhh/personal-site",
+    image: "",
+    description:
+      "This sites github repository. Used for people to see how I created this site and use it as inspiration",
+  },
+  {
     title: "Project 1",
+    link: "",
     image: "",
     description: "tesing",
   },
   {
     title: "Project 1",
+    link: "",
     image: "",
     description: "tesing",
   },
   {
     title: "Project 1",
+    link: "",
     image: "",
     description: "tesing",
   },
   {
     title: "Project 1",
+    link: "",
     image: "",
     description: "tesing",
   },
   {
     title: "Project 1",
-    image: "",
-    description: "tesing",
-  },
-  {
-    title: "Project 1",
+    link: "",
     image: "",
     description: "tesing",
   },
@@ -45,8 +53,9 @@ const Projects = () => {
     <div className="container mx-auto grid auto-rows-auto grid-cols-3 gap-6">
       {githubProjects.map((project, index) => {
         return (
-          <Card
-            key={index}
+          <Link
+            href={project.link}
+            target="_blank"
             className={`row-span-1  ${
               index === 0
                 ? "col-span-3"
@@ -63,15 +72,17 @@ const Projects = () => {
                 : "col-span-3"
             } hover:-translate-y-2 hover:scale-105 hover:shadow-2xl hover:shadow-slate-950 duration-300 ease-in-out`}
           >
-            <CardHeader>
-              <CardTitle>{project.title}</CardTitle>
-              <CardDescription>{project.description}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Test</p>
-              <img src="" alt="" />
-            </CardContent>
-          </Card>
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle>{project.title}</CardTitle>
+                <CardDescription>{project.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Test</p>
+                <img src="" alt="" />
+              </CardContent>
+            </Card>
+          </Link>
         );
       })}
     </div>
