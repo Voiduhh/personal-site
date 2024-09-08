d534d9872716abd073a9490e780220c0a198f5ce
Voiduh <jpik98@yahoo.com>
2024-09-08 11:19:38 -0400

    Finished contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,60 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+
 const Contact = () => {
-  return <div>contact</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.5, duration: 0.5, ease: "easeInOut" },
+      }}
+      className="container mx-auto grid grid-cols-2 h-[80vh] py-12 xl:py-0"
+    >
+      <div className="flex flex-col w-full mx-auto justify-start items-start">
+        <h3 className="font-bold text-4xl mb-8">Contact Me</h3>
+        <ul>
+          <li className="flex gap-4 mb-6 justify-left items-center">
+            <MdEmail className="text-2xl" />
+            <p>jpik@yahoo.com</p>
+          </li>
+          <li className="flex gap-4 mb-6 justify-left items-center">
+            <FaLinkedinIn className="text-2xl" />
+            <p>@jacobpikul</p>
+          </li>
+          <li className="flex gap-4 mb-6 justify-left items-center">
+            <FaGithub className="text-2xl" />
+            <p>@Voiduhh</p>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center rounded-3xl h-full justify-center bg-[#003554]">
+        <div className="relative bg-black w-[300px] sm:w-[350px] group transition-all duration-700 aspect-video flex items-center justify-center">
+          <div className="transition-all flex flex-col items-center py-5 justify-start duration-300 group-hover:duration-1000 bg-white w-full h-full absolute group-hover:-translate-y-16">
+            <p className="text-xl sm:text-2xl font-semibold text-gray-500 font-serif">
+              Thank You!
+            </p>
+            <p className="px-10 text-[10px] sm:text-[12px] text-gray-700">
+              Be sure to contact me with any questions or inquiries.
+            </p>
+            <p className="font-serif text-[10px] sm:text-[12px text-gray-700">
+              Have a good rest of the day!
+            </p>
+          </div>
+          <button className="seal bg-accent text-black w-10 aspect-square rounded-full z-40 text-[10px] flex items-center justify-center font-semibold [clip-path:polygon(50%_0%,_80%_10%,_100%_35%,_100%_70%,_80%_90%,_50%_100%,_20%_90%,_0%_70%,_0%_35%,_20%_10%)] group-hover:opacity-0 transition-all duration-1000 group-hover:scale-0 group-hover:rotate-180 border-4 border-accent-secondary">
+            JP
+          </button>
+          <div className="tp transition-all duration-1000 group-hover:duration-100 bg-neutral-800 absolute group-hover:[clip-path:polygon(50%_0%,_100%_0,_0_0)] w-full h-full [clip-path:polygon(50%_50%,_100%_0,_0_0)]"></div>
+          <div className="lft transition-all duration-700 absolute w-full h-full bg-neutral-900 [clip-path:polygon(50%_50%,_0_0,_0_100%)]"></div>
+          <div className="rgt transition-all duration-700 absolute w-full h-full bg-neutral-800 [clip-path:polygon(50%_50%,_100%_0,_100%_100%)]"></div>
+          <div className="btm transition-all duration-700 absolute w-full h-full bg-neutral-900 [clip-path:polygon(50%_50%,_100%_100%,_0_100%)]"></div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Contact;
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -16,83 +16,58 @@ const githubProjects = [
     description:
       "This sites github repository. Used for people to see how I created this site and use it as inspiration",
   },
-  {
-    title: "Project 1",
-    link: "",
-    image: "unity_logo.png",
-    description: "tesing",
-  },
-  {
-    title: "Project 1",
-    link: "",
-    image: "unreal_logo.png",
-    description: "tesing",
-  },
-  {
-    title: "Project 1",
-    link: "",
-    image: "",
-    description: "tesing",
-  },
-  {
-    title: "Project 1",
-    link: "",
-    image: "",
-    description: "tesing",
-  },
-  {
-    title: "Project 1",
-    link: "",
-    image: "",
-    description: "tesing",
-  },
 ];
 
 const Projects = () => {
   return (
-    <div className="container mx-auto grid auto-rows-auto grid-cols-3 gap-6">
-      {githubProjects.map((project, index) => {
-        return (
-          <Link
-            key={index}
-            href={project.link}
-            target="_blank"
-            className={`row-span-1  ${
-              index === 0
-                ? "col-span-3"
-                : index === githubProjects.length - 1
-                ? "col-span-3"
-                : index % 4 === 0
-                ? "col-span-1"
-                : index % 4 === 1
-                ? "col-span-1"
-                : index % 4 === 2
-                ? "col-span-2"
-                : index % 4 === 3
-                ? "col-span-2"
-                : "col-span-3"
-            } hover:-translate-y-2 hover:scale-105 hover:shadow-2xl hover:shadow-slate-950 duration-300 ease-in-out`}
-          >
-            <Card>
-              <CardHeader>
-                <CardTitle>{project.title}</CardTitle>
-                <CardDescription>{project.description}</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <img
-                  className={
-                    project.image.length === 0
-                      ? "hidden"
-                      : "w-full h-24 object-scale-down"
-                  }
-                  src={project.image}
-                  alt=""
-                />
-              </CardContent>
-            </Card>
-          </Link>
-        );
-      })}
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold mb-6">
+        Projects I am working on or completed
+      </h1>
+      <div className=" grid auto-rows-auto grid-cols-3 gap-6">
+        {githubProjects.map((project, index) => {
+          return (
+            <Link
+              key={index}
+              href={project.link}
+              target="_blank"
+              className={`row-span-1  ${
+                index === 0
+                  ? "col-span-3"
+                  : index === githubProjects.length - 1
+                  ? "col-span-3"
+                  : index % 4 === 0
+                  ? "col-span-1"
+                  : index % 4 === 1
+                  ? "col-span-1"
+                  : index % 4 === 2
+                  ? "col-span-2"
+                  : index % 4 === 3
+                  ? "col-span-2"
+                  : "col-span-3"
+              } hover:-translate-y-2 hover:scale-105 hover:shadow-2xl hover:shadow-slate-950 duration-300 ease-in-out`}
+            >
+              <Card>
+                <CardHeader>
+                  <CardTitle>{project.title}</CardTitle>
+                  <CardDescription>{project.description}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <img
+                    className={
+                      project.image.length === 0
+                        ? "hidden"
+                        : "w-full h-24 object-scale-down"
+                    }
+                    src={project.image}
+                    alt=""
+                  />
+                </CardContent>
+              </Card>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
